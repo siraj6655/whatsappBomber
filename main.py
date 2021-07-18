e5d7764006489b45358765e180e2b967f1de29ff
Hasan Imam <46672438+Hasan2325dev@users.noreply.github.com>
2021-07-18 21:51:13 +0530

    code update on 18 july 2021
    updated x paths as per new WhatsApp update ;)

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ msg = input("Message: ")
 user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 user.click()
 
-msgBox = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+msgBox = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[1]/div/div[2]')
 
 for i in range(count):
     msgBox.send_keys(msg)
-    sendButton = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')
+    sendButton = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
     sendButton.click()
 
 
